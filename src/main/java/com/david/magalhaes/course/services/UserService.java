c7ec29b2789bddcb309a710f962f0190ea961489
David Magalhães <deivi_gf144@hotmail.com>
2021-10-13 22:59:03 -0400

    Service layer, component registration

---
+++ src/main/java/com/david/magalhaes/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.david.magalhaes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.david.magalhaes.course.entities.User;
+import com.david.magalhaes.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
