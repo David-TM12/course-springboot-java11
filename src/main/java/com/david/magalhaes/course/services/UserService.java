c308e6af2c7c78324f6fa3adb9d00b975d1801a6
DavidTM <deivi_gf144@hotmail.com>
2021-10-18 13:12:03 -0400

    Exception handling update

--- src/main/java/com/david/magalhaes/course/services/UserService.java
+++ src/main/java/com/david/magalhaes/course/services/UserService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import com.david.magalhaes.course.entities.User;
 import com.david.magalhaes.course.repositories.UserRepository;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj){
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
