89b333422a91147ede4c4fc1d5340bded2eb7ebf
David Magalhães <deivi_gf144@hotmail.com>
2021-10-17 23:28:47 -0400

    Exception handling - findById

---
+++ src/main/java/com/david/magalhaes/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.david.magalhaes.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id "+id);
+    }
+}
